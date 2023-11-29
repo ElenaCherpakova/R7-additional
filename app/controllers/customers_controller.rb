c860f6ab4b1b9b602681c1b1f3836699911862ba
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-11-29 01:59:00 -0500

    task 4.3

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -30,15 +30,6 @@ class CustomersController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-    # respond_to do |format|
-    #   if @customer.save
-    #     format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
-    #     format.json { render :show, status: :created, location: @customer }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @customer.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PATCH/PUT /customers/1 or /customers/1.json
@@ -49,23 +40,19 @@ class CustomersController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
-    # respond_to do |format|
-    #   if @customer.update(customer_params)
-    #     format.html { redirect_to customer_url(@customer), notice: "Customer was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @customer }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @customer.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # DELETE /customers/1 or /customers/1.json
   def destroy
-    @customer.destroy
-
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
+    begin
+      @customer.destroy
+      flash.notice = "The customer record was successfully deleted." 
+    rescue ActiveRecord::InvalidForeignKey
+      flash.notice = "That customer record could not be deleted because the customer has orders."
+    end
+    
+      respond_to do |format|
+      format.html { redirect_to customers_url }
       format.json { head :no_content }
     end
   end
